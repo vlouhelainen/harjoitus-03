bb3e25ccff51fbf0cda03fa90b0b5d13b1d3ce92
vlouhelainen <mcturbo.vjml@gmail.com>
2014-09-24 11:07:52 +0300

    Harjoitus 3
    Valtteri Louhelainen

---
+++ harj3b.cpp
@@ -0,0 +1,45 @@
+/*		Valtteri Louhelainen
+
+#include <iostream>
+using namespace std;
+void main (void)
+{
+cout << "Hei maailma!";
+}
+
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM! Nime‰ tiedosto harj3a.cpp nimelle!
+
+Muokkaa edellisest‰ ohjelmasta versio nimelle harj3b.cpp, jossa
+nimi ja osoite tulostetaan n‰yt‰lle seuraavasti:
+
+**********************************
+* *
+* Etunimi *
+* Sukunimi *
+* *
+*-------------------------------------- *
+* *
+* Asemakatu 7 *
+* 40520 JKL *
+* *
+***********************************/
+
+
+#include <iostream>
+using namespace std;
+void main(void) //p‰‰ohjelma alkaa
+{
+	cout <<"**********************************\n"
+		<< "*                                *\n"
+		<< "*            Valtteri            *\n"
+		<< "*           Louhelainen          *\n"
+		<< "*                                *\n"
+		<< "*--------------------------------*\n"
+		<< "*                                *\n"
+		<< "*       Polttolinja 19 D 26      *\n"
+		<< "*            40520 JKL           *\n"
+		<< "*                                *\n"
+		<< "**********************************\n";
+}
